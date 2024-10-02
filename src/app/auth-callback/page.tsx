a9755f8ffff1eaf42690c16839121b8a528cef0e
ayushssshhh <ayushssshhh@gmail.com>
2024-10-02 21:28:08 +0530

    fixLogin

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -26,7 +26,7 @@ const Page = () => {
     
     if (authData?.success === false) {
         // User is synced to the database
-        router.push('https://pdflexayush.kinde.com/auth/cx/_:nav&m:login&psid:fc4dcf7ea41645d8ba066817ed64c38e');
+        router.push('https://pdflexayush.kinde.com/auth/cx/_:nav&m:login&psid:01924df327391ae82507aadfc02bb848');
     }
 
     return (
